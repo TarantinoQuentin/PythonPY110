c5310dda0beb8a0912d4e89863de02e1dd285bb6
TarantinoQuentin <gregdenfv42@gmail.com>
2025-06-01 12:11:45 +0300

    Сделал шестую лабораторную

--- app_wishlist/urls.py
+++ app_wishlist/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import wishlist_view
+from .views import wishlist_view, wishlist_add_view_json, wishlist_del_view_json, wishlist_view_json, wishlist_del_view
 
 
 app_name = 'app_wishlist'
 
 urlpatterns = [
-    path('wishlist/', wishlist_view, name='wishlist_view'),  # Зарегистрируйте обработчик
+    path('wishlist/', wishlist_view, name='wishlist_view'),
+    path('wishlist/api/add/<id_product>', wishlist_add_view_json),
+    path('wishlist/api/del/<id_product>', wishlist_del_view_json),
+    path('wishlist/api/', wishlist_view_json),
+    path('wishlist/remove/<str:id_product>', wishlist_del_view, name='wishlist_remove')
 ]
